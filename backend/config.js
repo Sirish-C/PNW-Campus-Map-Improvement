fb48b322b266116d8b7c674186c1b8e206031f75
Sirish-C <schejerl@pnw.ed>
2023-11-08 21:49:10 -0600

    pnw-rest-api-backend

---
+++ backend/config.js
@@ -0,0 +1,13 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "104.194.77.119",
+      port: 3306,
+      user: "admin",
+      password: "Nggyu1(((",
+      database: "PNW_map",
+      connectTimeout: 60000
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
