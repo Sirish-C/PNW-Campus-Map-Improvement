25ae724f57bcda08ed47844482dd0ccecfd96ae3
rmuddapuram <146110423+rmuddapuram@users.noreply.github.com>
2023-11-28 02:03:30 -0600

    Add files via upload

---
+++ frontend/src/Components/DetailsPages/Nils.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nils  = () => (
+  <div style={{ padding: '20px', backgroundColor: '#e6f7ff', minHeight: '100vh' }}>
+    <h2>Nils K. Nelson Bioscience Innovation Building</h2>
+
+<h2>First Floor</h2>
+<ul>
+<li>College of Nursing </li>
+<li>Biological Science</li>
+</ul>
+
+  </div>
+);
+
+export default Nils;
