a14fdaff114db38b1c221bf7d7ac0cb148888859
rmuddapuram <146110423+rmuddapuram@users.noreply.github.com>
2023-11-28 02:05:38 -0600

    Add files via upload

--- frontend/src/Components/DetailsPages/Library.js
+++ frontend/src/Components/DetailsPages/Library.js
@@ -1,34 +1,31 @@
 import React from 'react';
 
-
-const Library = () => (
-  <div >
-    <h2>Student Union and Library Building</h2>
-    
+const SULB  = () => (
+  <div style={{ padding: '20px', backgroundColor: '#e6f7ff', minHeight: '100vh' }}>
+    <h2>Student Union Library Building(SULB)</h2>
+<h2>First Floor</h2>
 <ul>
-<li>PNW Welcome Center - 125</li>
-<li>Bookstore & Campus Shop - 110</li>
-<li>Cafeteria - 150</li>
-<li>Library - 200</li>
-<li>Testing Center - 318</li>
-<li>Career Center - 349</li>
-<li>Honors College - 313</li>
+<li>Bookstore & Campus Shop - room 110</li>
+<li>Cafeteria - room 150</li>
+<li>PNW Welcome Center - room 125</li>
+<li>Student Lounge - room 104</li>
+<li>The Point - room 101</li>
+  <a href="https://www.pnw.edu/getting-to-pnw/wp-content/uploads/sites/7/2020/01/student-union-library-sul-first-floor.pdf" target="_blank">First Floor</a>
+</ul>
+<h2>Second Floor</h2>
+<ul>
+<li>Library - room 200</li>
+</ul>
+<h2>Third Floor</h2>
+<ul>
+<li>Career Center - room 349</li>
+<li>The Chronicle - room 344H</li>
+<li>Honors College - room 313</li>
+<li>Testing Center - room 318</li>
+  <a href="https://www.pnw.edu/getting-to-pnw/wp-content/uploads/sites/7/2020/01/student-union-library-sul-third-floor.pdf" target="_blank">Third Floor</a>
 </ul>
 
-
-<h2>Interior Floor Plans</h2>
-<ul>
-    <li>
-   
-    <a href="https://www.pnw.edu/getting-to-pnw/wp-content/uploads/sites/7/2020/01/student-union-library-sul-first-floor.pdf" target="_blank">First Floor</a>
-    </li>
-    <li>
- 
-    <a href="https://www.pnw.edu/getting-to-pnw/wp-content/uploads/sites/7/2020/01/student-union-library-sul-third-floor.pdf" target="_blank">Third Floor</a>
-    </li>
-</ul>
   </div>
 );
 
-
-export default Library;
+export default SULB;
