8e39aefd09d2957eae81e135bc1835413813dbe3
vpolina1998 <145486362+vpolina1998@users.noreply.github.com>
2023-11-28 23:01:03 -0600

    Buildings
    Adding Inserting queries of Buildings Table

---
+++ backend/Buildings.sql
@@ -0,0 +1,16 @@
+SELECT * FROM pnwcampusmap.Buildings;
+insert into Buildings(building_id,building_short_name,building_name)values('1','ANDR','Edward D. Anderson Building');
+insert into Buildings(building_id,building_short_name,building_name)values('2','CLO','ClassRoom Office Building');
+insert into Buildings(building_id,building_short_name,building_name)values('3','CMEC','Commercialization & Manufacturing Excellence Center');
+insert into Buildings(building_id,building_short_name,building_name)values('4','FNRC','Fitness & Recreation Center');
+insert into Buildings(building_id,building_short_name,building_name)values('5','GYTE','Millard E .Gyte Building');
+insert into Buildings(building_id,building_short_name,building_name)values('6','IDET','David Roberts Center for Innovation and Design');
+insert into Buildings(building_id,building_short_name,building_name)values('7','LAWS','C H Lawshe hall');
+insert into Buildings(building_id,building_short_name,building_name)values('8','NILS','Nils K. Nelson Bioscience Innovation Building');
+insert into Buildings(building_id,building_short_name,building_name)values('9','PORT','Gene Stratton Porter Hall ');
+insert into Buildings(building_id,building_short_name,building_name)values('10','POTT','Andrew A. Potter Building');
+insert into Buildings(building_id,building_short_name,building_name)values('11','PWRS','Donald S. Powers Computer Education Building');
+insert into Buildings(building_id,building_short_name,building_name)values('12','SAB','Schneider Avenue Building ');
+insert into Buildings(building_id,building_short_name,building_name)values('13','SULB','Student Union & Library');
+insert into Buildings(building_id,building_short_name,building_name)values('14','UNSV','University Services Building');
+
