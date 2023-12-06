2ddefc84afb73b4d4cb2f348f2bf34836b0d8d6c
VarshiniDunnala <146109985+VarshiniDunnala@users.noreply.github.com>
2023-12-06 12:02:52 -0600

    Added "Room" info
    Added 'Room'

--- frontend/src/Components/DetailsPages/Anderson.js
+++ frontend/src/Components/DetailsPages/Anderson.js
@@ -6,18 +6,18 @@ const Anderson  = () => (
 
 <h2>First Floor</h2>
 <ul>
-<li>Department of Engineering Technology - 143</li>
-<li>Department of Computer Information Technology & Graphics - 145</li>
+<li>Department of Engineering Technology - Room 143</li>
+<li>Department of Computer Information Technology & Graphics - Room 145</li>
 </ul>
 <h2>Second Floor</h2>
 <ul>
-<li>College of Technology - 202</li>
-<li>Department of Construction Science & Organizational Leadership - 212</li>
+<li>College of Technology - Room 202</li>
+<li>Department of Construction Science & Organizational Leadership - Room 212</li>
   <a href="https://www.pnw.edu/getting-to-pnw/wp-content/uploads/sites/7/2020/01/anderson-second-floor.pdf" target="_blank">Second Floor</a>
 </ul>
 <h2>Third Floor</h2>
 <ul>
-<li>College of Business - 357</li>
+<li>College of Business - Room 357</li>
   <a href="https://www.pnw.edu/getting-to-pnw/wp-content/uploads/sites/7/2020/01/andersons-third-floor.pdf" target="_blank">Third Floor</a>
 </ul>
 
